3192b9c08144809c29dec9c2ce2c96772a655a1a
weipeng0703 <chenweipeng0703@gmail.com>
2021-10-12 21:03:06 +0800

    Uploaded 2021年10月12--21:03

---
+++ STL源码剖析/30-Binder2nd.cpp
@@ -0,0 +1,5 @@
+/*
+binder2nd绑定第二实参
+
+新型适配器 bind取代binder1st，binder2nd
+*/
---
+++ STL源码剖析/32-bind.cpp
@@ -0,0 +1,10 @@
+/*
+新型适配器 bind取代binder1st, binder2nd, bind1st, bind2nd
+
+std::bind可以绑定
+    1.functions
+    2.functors
+    3.member functions   _1必须是某个object地址  _1为占位符(placeholders)
+    4.data members       _1必须是某个object地址
+
+*/
