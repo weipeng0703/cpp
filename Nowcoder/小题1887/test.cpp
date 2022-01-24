db97070580647c72435b3d9eed5f6a853dafd053
weipeng0703 <chenweipeng0703@gmail.com>
2022-01-24 21:46:14 +0800

    2022-1-24_力扣101-树的前中后序遍历

--- Nowcoder/小题1887/test.cpp
+++ Nowcoder/小题1887/test.cpp
@@ -8,29 +8,28 @@
 #include<iomanip>
 using namespace std;
 
-#include <iostream>
-using namespace std;
-void (*function)(float);
+int main()
+{
+    int a = 248, b = 4;
+    int const c = 21;
 
-void One(float one)
-{ 
-    cout<<"1"<<endl;    
+    *c= 32;
+    return 0;
 }
-void Two(float two)
-{ 
-    cout<<"2"<<endl; 
-}
-void Three(float three)
-{ 
-    cout<<"3"<<endl; 
-}
-int main() 
-{  
-    float i=1,j=2,k=3;
-   function = One;
-   function(i);
-   function= Two;
-   function(j);
-   function = Three;
-   function(k);
-}
+
+// A选项 
+// static成员变量可在类定义内初始化，这是有可能的。
+// 普通的static成员类内定义，类外初始化
+// const staic 可以类内初始化。
+// B选项：
+// const成员变量必须在构造函数的初始化列表中初始化？//不对吧，const可以在定义时初始化啊！
+// //在类定义的时候，就对const成员变量进行赋值,可能使得所有的对象const变量都是同一个值，且不能更改
+// D选项： 
+// 数组可以用大括号初始化，且不会出warning。如：
+// class A{
+//     private:
+//         int a[100];
+//     public:
+//         A(int a0,int a1,int a2):a{a0,a1,a2}{
+//         }
+// }
---
+++ code/leetcode/力扣101/3-数据结构/ch14-树(指针三剑客之二)/13.3-前中后序遍历(深度优先搜索) copy/144-二叉树的前序遍历(中后序类似).cpp
@@ -0,0 +1,74 @@
+/*
+给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+示例一：
+    输入：root = [1,null,2,3]
+    输出：[1,2,3]
+*/
+#include<vector> 
+#include<algorithm>
+#include<unordered_map>
+#include<string>
+#include<stack>
+#include<priority_queue>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// 1.递归
+class Solution {
+public:
+    void preorder(TreeNode* root, vector<int> &v)
+    {
+        if (root == nullptr)
+        {
+            return; 
+        }
+        v.push_back(root->val);
+        preorder(root->left, v);
+        preorder(root->right, v);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        preorder(root, res);
+        return res;
+    }
+};
+// 2.迭代（非递归方式）
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr)
+        {
+            return res;
+        }
+        stack<TreeNode*> temp;
+        temp.push(root);
+        while (!temp.empty())
+        {
+            TreeNode* node = temp.top();
+            res.push_back(node->val);
+            temp.pop();
+            // 注意：栈式先进后出，所以要先访问左子树要先把左子树后放入
+            if (node->right != nullptr)
+            {
+                stack.push(node->right);
+            }
+            if (node->left != nullptr)
+            {
+                stack.push(node->left);
+            }
+        }
+        return res;
+    }
+};
