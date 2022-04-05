38f4c40305e3b3c6c73fa5d191b55bc0d7a42bda
weipeng0703 <chenweipeng0703@gmail.com>
2022-04-05 10:43:41 +0800

    2022-4-5_代码随想录-字符串day1

--- Nowcoder/小题1887/test.cpp
+++ Nowcoder/小题1887/test.cpp
@@ -10,14 +10,13 @@
 #include <memory>
 using namespace std; 
 
-int n = 14;
-double x = 2.71828;
-std::string mr = "Mr.String objects!";
-show_list(n, x);
-//这里的目标是定义show_list()
-show_list(x*x, '!', 7, mr);
-/*    
-运行结果：
-    14, 2.71828
-    7.38905， !, 7, Mr.String objects!
-*/  
+int main() {
+
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        count += i;
+    }
+    cout << "count = " << count << endl;
+    return 0;
+}
---
+++ code/代码随想录/4-字符串/2-反转字符串(2).cpp
@@ -0,0 +1,43 @@
+/*
+力扣541. 反转字符串 II
+给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+
+示例 1：
+    输入：s = "abcdefg", k = 2
+    输出："bacdfeg"
+示例 2：
+    输入：s = "abcd", k = 2
+    输出："bacd"
+*/ 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+#include<stack>
+#include<set>
+#include<vector>
+#include<stdio.h>
+#include<iomanip>
+#include<algorithm>
+#include<unordered_map>
+#include<priority_queue>
+using namespace std; 
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k))
+        {
+            if (i + k <= s.size()) {
+                reverse(s.begin() + i, s.begin() + i + k);
+                continue;
+            }
+            // 剩余字符小于k个，全部反转
+            // reverse(s.begin() + i, s.begin() + s.size());
+            reverse(s.begin() + i, s.end());
+        }
+        return s;
+    }
+};
