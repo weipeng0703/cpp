9121c10b54d4477ec11a80e6b394e3396655a598
weipeng0703 <chenweipeng0703@gmail.com>
2022-05-19 11:23:01 +0800

    2022-5-19_代码随想录-dpDay9-买卖股票finish

---
+++ 刷题/代码随想录/10-dp/4-股票问题/39-买卖股票含手续费.cpp
@@ -0,0 +1,86 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: weipeng
+ * @Date: 2022-05-19 11:08:59
+ * @LastEditors: weipeng
+ * @LastEditTime: 2022-05-19 11:18:28
+ */
+/*
+力扣121. 买卖股票的最佳时机
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+122. 买卖股票的最佳时机 II (股票可以无限次买入卖出)
+给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+返回 你能获得的 最大 利润 。
+
+123. 买卖股票的最佳时机 III (股票最多买入卖出两次)
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+188. 买卖股票的最佳时机 IV (股票三的泛化——股票最多买入卖出K次)
+给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+309. 最佳买卖股票时机含冷冻期
+给定一个整数数组prices，其中第 prices[i] 表示第 i 天的股票价格 。​
+设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+714. 买卖股票的最佳时机含手续费
+给定一个整数数组 prices，其中 prices[i]表示第 i 天的股票价格 ；整数 fee 代表了交易股票的手续费用。
+你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+返回获得利润的最大值。
+注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+
+示例 1：
+    输入：prices = [1, 3, 2, 8, 4, 9], fee = 2
+    输出：8
+    解释：能够达到的最大利润:  
+    在此处买入 prices[0] = 1
+    在此处卖出 prices[3] = 8
+    在此处买入 prices[4] = 4
+    在此处卖出 prices[5] = 9
+    总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8
+示例 2：
+    输入：prices = [1,3,7,5,10,3], fee = 3
+    输出：6
+*/ 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+#include<stack>
+#include<set>
+#include<vector>
+#include<stdio.h>
+#include<iomanip>
+#include<algorithm>
+#include<unordered_map>
+#include<priority_queue>
+using namespace std; 
+
+// 1.dp
+// 跟买卖股票二几乎完全相同
+// 只需要在每次卖出股票时减去一次手续费即可
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        if (prices.size() == 0) return 0;
+        vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+        }
+        return dp[prices.size() - 1][1];
+    }
+};
