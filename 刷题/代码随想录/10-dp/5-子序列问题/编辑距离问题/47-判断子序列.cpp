f5810ad3106aafc6a5d8edd287bfc9a5e005ba40
weipeng0703 <chenweipeng0703@gmail.com>
2022-05-22 09:37:35 +0800

    2022-5-21_代码随想录-dpDay11-编辑距离问题day1

--- 刷题/代码随想录/10-dp/5-子序列问题/编辑距离问题/47-判断子序列.cpp
+++ 刷题/代码随想录/10-dp/5-子序列问题/编辑距离问题/47-判断子序列.cpp
@@ -4,7 +4,7 @@
  * @Author: weipeng
  * @Date: 2022-05-21 10:31:14
  * @LastEditors: weipeng
- * @LastEditTime: 2022-05-21 11:19:37
+ * @LastEditTime: 2022-05-22 09:37:05
  */
 
 /*
@@ -40,25 +40,8 @@ using namespace std;
 // 思路：
 // 求s与t的公共子序列的长度len（与T44一模一样）,
 // 如果len == s.size()证明s是子序列，否则不是
-// 1. dp
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-        if (t.size() < s.size()) return false;
-        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
-        for (int i = 1; i <= s.size(); i++) {
-            for (int j = 1; j <= t.size(); j++) {
-                if (s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
-                // 因为是判断s是否为t的子序列；下面那种写法不能这样写，
-                // 因为那是求两个序列中的最长子序列， 不涉及谁是谁的子序列
-                else dp[i][j] = dp[i][j - 1];   
-            }
-        }
-        return dp[s.size()][t.size()] == s.size();
-    }
-};
 
-// 2. T43写法
+// 1. 按照T44（求最长公共子序列）的写法
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
@@ -77,3 +60,22 @@ public:
         return ans == s.size();
     }
 };
+
+// 2. 本题独有写法
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (t.size() < s.size()) return false;
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= t.size(); j++) {
+                if (s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                // 因为是判断s是否为t的子序列，所以不能延续上面的写法：
+                // 因为那是求两个序列中的最长公共子序列，不涉及谁是谁的子序列
+                else dp[i][j] = dp[i][j - 1];
+            }
+        }
+        return dp[s.size()][t.size()] == s.size();
+    }
+};
+
