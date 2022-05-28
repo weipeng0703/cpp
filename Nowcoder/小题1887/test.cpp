c0597d3e27fbce391d229af5bc522181edeb7240
weipeng0703 <chenweipeng0703@gmail.com>
2022-05-28 22:18:21 +0800

    2022-5-28_面经题目到C++_T26&&计网_6

--- Nowcoder/小题1887/test.cpp
+++ Nowcoder/小题1887/test.cpp
@@ -4,7 +4,7 @@
  * @Author: weipeng
  * @Date: 2022-01-11 20:11:55
  * @LastEditors: weipeng
- * @LastEditTime: 2022-05-27 23:00:04
+ * @LastEditTime: 2022-05-28 17:44:19
  */
 #include<stdio.h>
 #include<stdlib.h>
@@ -30,33 +30,25 @@ using namespace std;
 
 #include <stdio.h>
 
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-class ADT {
-    int i;
-    int j;
-public:
-    ADT() {
-        i = 10;
-        j = 100;
-        cout << "ADT construct i = " << i << ", j = "<<j <<endl;
-    }
-    ~ADT(){
-        cout << "ADT destruct" << endl;
-    }
-};
-int main() {
-    char *p = new(nothrow) char[sizeof(ADT) + 1];
-    if (p == NULL) cout << "alloc failed" << endl;
-    ADT *q = new(p) ADT; //placement new:不必担心失败，只要p所指对象的的空间足够ADT创建即可
-    //delete q;//错误!不能在此处调用delete q;
-    q->ADT::~ADT();//显示调用析构函数
-    delete[] p;
-    return 0; 
+
+template<typename T> int printType(const T& a) ;
+
+template<>
+int printType(const int& a) {
+    return (typeid(a) == typeid(int))? 1 : 0;
 }
-//输出结果：
-// ADT construct i = 10, j = 100
-// ADT destruct
+
+int main(){
+    int a = printType(1.0);
+    if (a == 1) std::cout << "i am int" << std::endl;
+    else std::cout << "i am others" << std::endl;
+}
+
+
