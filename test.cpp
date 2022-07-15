b872897fae277a06fe62df55b5c6c2f9a69af444
weipeng0703 <chenweipeng0703@gmail.com>
2022-07-15 22:34:45 +0800

    RAII与智能指针

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
  * @Author: weipeng
  * @Date: 2022-06-27 09:44:14
  * @LastEditors: weipeng
- * @LastEditTime: 2022-06-30 23:03:44
+ * @LastEditTime: 2022-07-14 15:18:29
  */
 # include<vector> 
 # include<algorithm>
@@ -16,34 +16,39 @@
 # include<thread>
 #include<windows.h>
 #include <cstdlib>
-using namespace std;
-
 #include <vector>
 #include <string>
 #include <iostream>
 using namespace std;
-struct Person {
-    string name;
-    int age;
-    //初始构造函数
-    Person(string p_name, int p_age): name(std::move(p_name)), age(p_age) {
-        cout << "I have been constructed" << endl;
-    }
-    //拷贝构造函数
-    Person(const Person& other): name(std::move(other.name)), age(other.age) {
-        cout << "I have been copy constructed" << endl;
-    }
-    //转移构造函数
-    Person(Person&& other): name(std::move(other.name)), age(other.age) {
-        cout << "I have been moved" << endl;
-    }
-};
+
 int main() {
-    vector<Person> e;
-    cout << "emplace_back:" << endl;
-    e.emplace_back("Jane", 23); //不用构造类对象
-    vector<Person> p;
-    cout << "push_back:"<< endl;
-    p.push_back(Person("Mike", 36));
+    // auto basicLambda = [] {cout << "hello world!" << endl; };
+    // basicLambda();
+
+    // auto add = [] (int a, int b) -> int {return a + b;};
+    // auto cheng = [] (int a, int b)  {return a * b;};
+
+    // int sum = add(2, 5);
+    // int chengyi = cheng(2, 5);
+
+    // cout << sum << endl;
+    // cout << chengyi << endl;
+
+    // int x = 10;
+    // auto addX = [x] (int a) {return a + x;};
+
+    // auto chengX = [&x] (int a) {return a * x;};
+
+    // cout << addX(2) << endl;
+    // cout << chengX(2) << endl;
+
+    // int val = 3;
+    // vector<int> ans {1,2,3,4,5,6,7,8,9};
+    // int count = std::count_if(ans.begin(), ans.end(), [val] (int x) {return x > val;});
+    // cout << count << endl;
+
+    constexpr int getFive() { return 5; };
+    int arr[getFive() + 1];
+
     return 0; 
 }
