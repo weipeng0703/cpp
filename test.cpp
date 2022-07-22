bbec1d38be22af4657b1352884ac48ccaddd2d42
weipeng0703 <chenweipeng0703@gmail.com>
2022-07-22 21:13:14 +0800

    update 八股

--- test.cpp
+++ test.cpp
@@ -4,8 +4,25 @@
  * @Author: weipeng
  * @Date: 2022-06-27 09:44:14
  * @LastEditors: weipeng
- * @LastEditTime: 2022-07-14 15:18:29
+ * @LastEditTime: 2022-07-22 20:37:46
  */
+# include<vector> 
+# include<algorithm>
+# include<unordered_map>
+# include<string>
+# include<stack>
+# include<queue>
+# include<iostream>
+# include<thread>
+#include<windows.h>
+#include <cstdlib>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
 # include<vector> 
 # include<algorithm>
 # include<unordered_map>
@@ -16,39 +33,56 @@
 # include<thread>
 #include<windows.h>
 #include <cstdlib>
-#include <vector>
-#include <string>
-#include <iostream>
 using namespace std;
 
+pthread_mutex_t mutex_A = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_B = PTHREAD_MUTEX_INITIALIZER;
+
+//线程函数 A
+void *threadA_proc(void *data) {
+    printf("thread A waiting get ResourceA \n");
+    pthread_mutex_lock(&mutex_A);
+    printf("thread A got ResourceA \n");
+    
+    Sleep(1);
+    
+    printf("thread A waiting get ResourceB \n");
+    pthread_mutex_lock(&mutex_B);
+    printf("thread A got ResourceB \n");
+
+    pthread_mutex_unlock(&mutex_B);
+    pthread_mutex_unlock(&mutex_A);
+    return (void *)0;
+}
+
+//线程函数 B
+void *threadB_proc(void *data) {
+    printf("thread B waiting get ResourceB \n");
+    pthread_mutex_lock(&mutex_B);
+    printf("thread B got ResourceB \n");
+    
+    Sleep(1);
+    
+    printf("thread B waiting  get ResourceA \n");
+    pthread_mutex_lock(&mutex_A);
+    printf("thread B got ResourceA \n");
+    
+    pthread_mutex_unlock(&mutex_A);
+    pthread_mutex_unlock(&mutex_B);
+    return (void *)0;
+}
+
 int main() {
-    // auto basicLambda = [] {cout << "hello world!" << endl; };
-    // basicLambda();
-
-    // auto add = [] (int a, int b) -> int {return a + b;};
-    // auto cheng = [] (int a, int b)  {return a * b;};
-
-    // int sum = add(2, 5);
-    // int chengyi = cheng(2, 5);
-
-    // cout << sum << endl;
-    // cout << chengyi << endl;
-
-    // int x = 10;
-    // auto addX = [x] (int a) {return a + x;};
-
-    // auto chengX = [&x] (int a) {return a * x;};
-
-    // cout << addX(2) << endl;
-    // cout << chengX(2) << endl;
-
-    // int val = 3;
-    // vector<int> ans {1,2,3,4,5,6,7,8,9};
-    // int count = std::count_if(ans.begin(), ans.end(), [val] (int x) {return x > val;});
-    // cout << count << endl;
-
-    constexpr int getFive() { return 5; };
-    int arr[getFive() + 1];
-
-    return 0; 
+    pthread_t tidA, tidB;
+    
+    //创建两个线程
+    pthread_create(&tidA, NULL, threadA_proc, NULL);
+    pthread_create(&tidB, NULL, threadB_proc, NULL);
+    
+    pthread_join(tidA, NULL);
+    pthread_join(tidB, NULL);
+    
+    printf("exit\n");
+    
+    return 0;
 }
