fb3effa31fa6f2268e3b6353ee060e1b1dcff56f
weipeng0703 <chenweipeng0703@gmail.com>
2022-07-30 22:09:00 +0800

    接雨水&轮转数组

---
+++ 刷题/CodeTop/字节/189-轮转数组.cpp
@@ -0,0 +1,50 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: weipeng
+ * @Date: 2022-07-30 21:54:24
+ * @LastEditors: weipeng
+ * @LastEditTime: 2022-07-30 22:08:18
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+#include<stack>
+#include<set>
+#include<vector>
+#include<stdio.h>
+#include<iomanip>
+#include<algorithm>
+#include<unordered_map>
+#include<priority_queue>
+using namespace std;
+/*
+力扣T-189. 轮转数组
+给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+
+示例 1:
+    输入: nums = [1,2,3,4,5,6,7], k = 3
+    输出: [5,6,7,1,2,3,4]
+    解释:
+    向右轮转 1 步: [7,1,2,3,4,5,6]
+    向右轮转 2 步: [6,7,1,2,3,4,5]
+    向右轮转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+    输入：nums = [-1,-100,3,99], k = 2
+    输出：[3,99,-1,-100]
+    解释: 
+    向右轮转 1 步: [99,-1,-100,3]
+    向右轮转 2 步: [3,99,-1,-100]
+
+*/
+// 1-reverse
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
